df1a51726986b25c6d20c6b7c572e84c82e46f2e
Yakimov Vyacheslav <js-nanodegree@outlook.com>
2021-11-05 17:03:19 +0300

    fix optimize FlatList

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import React from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {Provider} from 'react-redux';
 
 import ScreensStack from './src/screens';
-import {store} from './src/store';
 
 
 const App = () => {
@@ -14,24 +12,22 @@ const App = () => {
 
   const backgroundStyle = {
     'backgroundColor': isDarkMode ?
-Colors.darker :
-Colors.lighter,
+        Colors.darker :
+        Colors.lighter,
     'flex': 1,
   };
   return (
-      <Provider store={store}>
-          <PaperProvider>
-              <SafeAreaView style={backgroundStyle}>
-                  <StatusBar
-                      barStyle={isDarkMode ?
+      <PaperProvider>
+          <SafeAreaView style={backgroundStyle}>
+              <StatusBar
+                  barStyle={isDarkMode ?
 'light-content' :
 'dark-content'}
-                  />
+              />
 
-                  <ScreensStack />
-              </SafeAreaView>
-          </PaperProvider>
-      </Provider>
+              <ScreensStack />
+          </SafeAreaView>
+      </PaperProvider>
   );
 };
 
--- src/screens/List/ListScreen/Screen.js
+++ src/screens/List/ListScreen/Screen.js
@@ -1,14 +1,19 @@
+/* eslint-disable import/named */
 /* eslint-disable no-param-reassign */
 import React from 'react';
 
 import {FlatList, View, StyleSheet, RefreshControl} from 'react-native';
 import {Text, useTheme} from 'react-native-paper';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import ListItem from '../ListItem';
+
+const ITEM_HEIGHT = 100
 
 export function Screen({handleRefresh, list, error, loading, time}) {
     const {colors} = useTheme();
     const ref = React.useRef(null)
+    const insets = useSafeAreaInsets()
 
     const EmptyList = () => (
         <React.Suspense fallback={View}>
@@ -26,6 +31,14 @@ export function Screen({handleRefresh, list, error, loading, time}) {
 
     const RenderItem = React.useCallback(({item}) => (<ListItem item={item} />), [list]);
 
+    const GetItemLayout = React.useCallback((data, index) => (
+        {
+            index,
+            'length': ITEM_HEIGHT,
+            'offset': ITEM_HEIGHT * index,
+        }
+    ), [list])
+
     const Message = () => (
         <Text>
             {error}
@@ -37,10 +50,15 @@ export function Screen({handleRefresh, list, error, loading, time}) {
             style={[{'backgroundColor': colors.background}, styles.blockStyle]}
         >
             <FlatList
+                removeClippedSubviews
                 ListEmptyComponent={EmptyList}
                 ListHeaderComponent={Message}
-                contentContainerStyle={styles.stylesFlex}
+                contentContainerStyle={[styles.stylesFlex, {'marginBottom': insets.bottom}]}
                 data={list}
+                extraData={list}
+                getItemLayout={GetItemLayout}
+                initialNumToRender={10}
+                maxToRenderPerBatch={16}
                 ref={ref}
                 refreshControl={
                     <RefreshControl
@@ -51,6 +69,7 @@ export function Screen({handleRefresh, list, error, loading, time}) {
                     />
                 }
                 renderItem={RenderItem}
+                updateCellsBatchingPeriod={100}
             />
         </View>
     );
